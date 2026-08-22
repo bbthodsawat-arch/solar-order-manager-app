7d7f5a7365eee117ab8d0768fd63f65dabab188e
bbthodsawat-arch <b.b.thodsawat@gmail.com>
2026-08-23 01:39:09 +0700

    refactor(command-center): route business modules through migration workspace

--- src/pages/UnifiedCommandCenter.tsx
+++ src/pages/UnifiedCommandCenter.tsx
@@ -1,9 +1,5 @@
-import CommandCenter from './CommandCenter';
-import POSCommandCenterPanel from '../components/pos/POSCommandCenterPanel';
-import BrandPaymentWorkspace from '../components/BrandPaymentWorkspace';
 import { CommandCenterShell } from '../features/command-center/CommandCenterShell';
-import { LegacyCommandAdapter } from '../features/command-center/LegacyCommandAdapter';
-import { getCommand } from '../features/command-center/registry';
+import { BusinessMigrationWorkspace } from '../features/command-center/BusinessMigrationWorkspace';
 
 interface UnifiedCommandCenterProps {
   onNavigateToUsers?: () => void;
@@ -12,21 +8,9 @@ interface UnifiedCommandCenterProps {
 }
 
 export default function UnifiedCommandCenter(props: UnifiedCommandCenterProps) {
-  const businessCommand = getCommand('business.profile');
-  const catalogCommand = getCommand('catalog.products');
   return (
     <CommandCenterShell>
-      <div className="space-y-5">
-        <LegacyCommandAdapter command={businessCommand!}>
-          <BrandPaymentWorkspace />
-        </LegacyCommandAdapter>
-        <LegacyCommandAdapter command={catalogCommand!}>
-          <POSCommandCenterPanel />
-        </LegacyCommandAdapter>
-        <LegacyCommandAdapter command={businessCommand!}>
-          <CommandCenter {...props} />
-        </LegacyCommandAdapter>
-      </div>
+      <BusinessMigrationWorkspace {...props} />
     </CommandCenterShell>
   );
 }
