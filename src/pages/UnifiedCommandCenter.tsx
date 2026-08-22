5f9201374ba5edd6f3d56c778c4bdd4b964e16c3
bbthodsawat-arch <b.b.thodsawat@gmail.com>
2026-08-23 01:40:15 +0700

    feat(command-center): wire registry navigation into business migration

--- src/pages/UnifiedCommandCenter.tsx
+++ src/pages/UnifiedCommandCenter.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { CommandCenterShell } from '../features/command-center/CommandCenterShell';
-import { BUSINESS_MIGRATION_COMMANDS, BusinessMigrationWorkspace } from '../features/command-center/BusinessMigrationWorkspace';
+import { BUSINESS_MIGRATION_COMMANDS, BusinessMigrationWorkspace, type BusinessMigrationCommand } from '../features/command-center/BusinessMigrationWorkspace';
+import type { CommandDefinition } from '../features/command-center/registry';
 
 interface UnifiedCommandCenterProps {
   onNavigateToUsers?: () => void;
@@ -8,9 +10,16 @@ interface UnifiedCommandCenterProps {
 }
 
 export default function UnifiedCommandCenter(props: UnifiedCommandCenterProps) {
+  const [activeCommand, setActiveCommand] = useState<BusinessMigrationCommand>('business.brand');
+  const handleCommand = (command: CommandDefinition) => {
+    if (BUSINESS_MIGRATION_COMMANDS.some(item => item.id === command.id)) {
+      setActiveCommand(command.id as BusinessMigrationCommand);
+    }
+  };
+
   return (
-    <CommandCenterShell additionalCommands={BUSINESS_MIGRATION_COMMANDS}>
-      <BusinessMigrationWorkspace {...props} />
+    <CommandCenterShell additionalCommands={BUSINESS_MIGRATION_COMMANDS} onSelectCommand={handleCommand}>
+      <BusinessMigrationWorkspace {...props} activeCommand={activeCommand} onActiveCommandChange={setActiveCommand} />
     </CommandCenterShell>
   );
 }
